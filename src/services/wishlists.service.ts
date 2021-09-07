189b7d33b8a4eac6c1559d12a21fa54aefe57efa
João Paulo Marquesini <jao@markezine.me>
2021-09-07 13:04:34 -0300

    fixes delete and create wishlist methods

--- src/services/wishlists.service.ts
+++ src/services/wishlists.service.ts
@@ -1,25 +1,25 @@
 import db from "./database.service";
 import Wishlist from "../interfaces/wishlist.interface";
 
-export async function createWishlist(wishlist: Wishlist):Promise<Wishlist> {
-    let key = await db.wishlists?.add(wishlist).then(r=>r).catch(e=>console.log);
-    console.log(key);
+export async function createWishlist(wishlist: Wishlist): Promise<Wishlist> {
+    let key = await db.wishlists?.add(wishlist);
     return {
         ...wishlist,
-        id: 1
+        id: key
     };
 }
 
-export async function deleteWishlist(wishlist: Wishlist):Promise<void> {
+export async function deleteWishlist(wishlist: Wishlist): Promise<void> {
     await db.wishlists?.delete(wishlist.id!);
+    await db.wishlistBuilds.where('wishlistId').equals(wishlist.id).delete();
 }
 
-export async function getWishlist(id: number):Promise<Wishlist|undefined> {
+export async function getWishlist(id: number): Promise<Wishlist | undefined> {
     let wishlist = await db.wishlists?.get(id);
     return wishlist;
 }
 
-export async function getAllWishlists():Promise<Wishlist[]|undefined> {
+export async function getAllWishlists(): Promise<Wishlist[] | undefined> {
     let wishlists = await db.wishlists?.toArray();
     return wishlists;
 }
